c660d01998fcc3bf88ae54c823d4c4c0069b324b
ledoge <s3915203@rmit.edu.vn>
2023-07-28 19:39:45 +0700

    asdasd'

---
+++ assignment1/CustomView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomView.swift
+//  assignment1
+//
+//  Created by ninnix on 28/07/2023.
+//
+
+import SwiftUI
+
+struct CustomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomView()
+    }
+}
--- assignment1/NavigationView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NavigationView.swift
-//  assignment1
-//
-//  Created by Bao Luong Gia on 24/07/2023.
-//
-
-import SwiftUI
-
